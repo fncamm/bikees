1662728498a8fd40e4770d0c616d59d9da23445c
fncamm <sapgreeen@gmail.com>
2017-12-29 22:11:29 +0200

    fix coding style

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,48 @@
-$(document).ready(()=>{
+/* global $  */
+
+function moveBackground(event, backgroundImage, header, secondary) {
+  const offset = backgroundImage.offset();
+  const halfWidth = backgroundImage.width() / 2;
+  const halfHeight = backgroundImage.height() / 2;
+  const centerX = halfWidth + offset.left;
+  const centerY = halfHeight + offset.top;
+  const mx = event.pageX - centerX;
+  const my = event.pageY - centerY;
+  const offsetX = (mx / halfWidth);
+  const offsetY = (my / halfHeight);
+
+  backgroundImage.css({ left: `${offsetX}%`, top: `${offsetY}%` });
+  header.css({ left: `${offsetX / 2}%`, top: `${offsetY / 2}%` });
+  secondary.css({ left: `${offsetX / 3}%`, top: `${offsetY / 3}%` });
+}
+
+$(document).ready(() => {
   const searchInput = $('.search');
-  searchInput.click((e)=>{
-    e.stopPropagation();
+  searchInput.click((event) => {
+    event.stopPropagation();
   });
-  $(document).click((e)=>{
+
+  $(document).click(() => {
     searchInput.removeClass('active');
     searchInput.val('');
   });
-  $('.search-icon').click((e)=>{
+
+  $('.search-icon').click((event) => {
     searchInput.toggleClass('active');
     if (searchInput.hasClass('active')) {
       searchInput.focus();
     }
-    e.stopPropagation();
+    event.stopPropagation();
   });
 
-  const hisperBgImg = $('.bg-hisper-image');
-  const hisperHeader = $('.hisper-header');
-  const hisperSecondary = $('.hisper-secondary');
-  const handler = (e)=>moveBg(e, hisperBgImg, hisperHeader, hisperSecondary)
-  hisperBgImg.mousemove(handler);
-  $('.hisper').mousemove(handler);
+  const $hisperBackgroundImage = $('.bg-hisper-image');
+  const hisperHandler = e => moveBackground(e, $hisperBackgroundImage, $('.hisper-header'), $('.hisper-secondary'));
+  $hisperBackgroundImage.mousemove(hisperHandler);
+  $('.hisper').mousemove(hisperHandler);
 
-  const hippieBgImg = $('.bg-hippie-image');
-  const hippieHeader = $('.hippie-header');
-  const hippieSecondary = $('.hippie-secondary');
-  const handler2 = (e)=>moveBg(e, hippieBgImg, hippieHeader, hippieSecondary)
-  hippieBgImg.mousemove(handler2);
-  $('.hippie').mousemove(handler2);
-
-  function moveBg(e, bgImg, header, secondary) {
-    const offset = bgImg.offset();
-    const halfWidth = bgImg.width()/2;
-    const halfHeight = bgImg.height()/2;
-    const centerX = halfWidth + offset.left;
-    const centerY = halfHeight + offset.top;
-    const mx = e.pageX-centerX;
-    const my = e.pageY-centerY;
-    const offsetX = (mx/halfWidth * 1);
-    const offsetY = (my/halfHeight * 1);
-    bgImg.css({'left': `${offsetX}%`, 'top': `${offsetY}%`});
-    header.css({'left': `${offsetX/2}%`, 'top': `${offsetY/2}%`});
-    secondary.css({'left': `${offsetX/3}%`, 'top': `${offsetY/3}%`})
-    //console.log($(bgHisper).offset(), e.pageX, e.pageY)
-  }
+  const $hippieBackgroundImage = $('.bg-hippie-image');
+  const hippieHandler = e => moveBackground(e, $hippieBackgroundImage, $('.hippie-header'), $('.hippie-secondary'));
+  $hippieBackgroundImage.mousemove(hippieHandler);
+  $('.hippie').mousemove(hippieHandler);
 });
 
